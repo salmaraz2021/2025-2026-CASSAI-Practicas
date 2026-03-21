2944cf6030a0fc0735d3b79b6132ebe78bdec914
salmaraz2021 <s.almaraz.2021@alumnos.urcj.es>
2026-03-21 15:23:37 +0100

    cambios

--- P2/crono.js
+++ P2/crono.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 class Crono {
     constructor(display) {
         this.display = display;
--- P2/script.js
+++ P2/script.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 // --- ELEMENTOS ---
 const d1 = document.getElementById("d1")
 const d2 = document.getElementById("d2")
